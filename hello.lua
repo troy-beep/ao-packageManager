7f01f355d2e0d4f9c9c2ad4e3b7a3fd2ea84d86a
troy-beep <krish.disbound@gmail.com>
2024-12-06 23:10:37 +0530

    Update hello.lua

--- hello.lua
+++ hello.lua
@@ -3,3 +3,11 @@ Handlers.add(
   Handlers.utils.hasMatchingData("ping"),
   Handlers.utils.reply("pong")
 )
+
+-- sends a message to another process ("Transfer" action)
+ao.send({
+  Target = "usjm4PCxUd5mtaon7zc97-dt-3qf67yPyqgzLnLqk5A",
+  Action = "Transfer",
+  Recipient = "XjvCPN31XCLPkBo9bUeB7vAK0VC6-eY52-CS-6Iho8F",
+  Quantity = tostring(1045)
+})
