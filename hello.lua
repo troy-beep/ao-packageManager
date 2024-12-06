7499daefe5e934a741d8af8f16c3a921e60dbc4d
troy-beep <krish.disbound@gmail.com>
2024-12-06 23:14:29 +0530

    Update hello.lua

--- hello.lua
+++ hello.lua
@@ -11,3 +11,13 @@ ao.send({
   Recipient = "XjvCPN31XCLPkBo9bUeB7vAK0VC6-eY52-CS-6Iho8F",
   Quantity = tostring(1045)
 })
+
+local str = "ao"
+
+local iv = "super_secret_shh"
+local key_128 = "super_secret_shh"
+
+local encrypted = crypto.cipher.aes.encrypt("ao", key, iv).asHex() -- A3B9E6E1FBD9D46930E5F76807C84B8E
+local decrypted = crypto.cipher.aes.decrypt(encrypted, key, iv).asHex() -- 616F0000000000000000000000000000
+
+crypto.utils.hex.hexToString(decrypted) -- ao
